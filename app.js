8ff8ea9521d444cc088cd311c57bcf1619eeec02
xs-web-lyq <3235951041@qq.com>
2023-10-28 16:21:21 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const log4js = require('./utils/log4j')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+// 初始化配置的日志中间件----- 太过于温和所以使用了 log4js 插件进行日志打印
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+
+// logger
+app.use(async (ctx, next) => {
+  await next()
+  log4js.info(`log output`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  log4js.error(`${err.stack}`)
+});
+
+module.exports = app
